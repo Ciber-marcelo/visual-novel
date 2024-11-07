657b7b09a8b8356d8f50da66a4d65f5ee07c9ff3
Ciber-marcelo <marcelones125@gmail.com>
2024-11-06 23:51:24 -0300

    chore: edit MenuMain

--- src/components/MenuMain/ButtonMenu/index.tsx
+++ src/components/MenuMain/ButtonMenu/index.tsx
@@ -5,11 +5,23 @@ type ButtonMenuProps = {
 
 export default function ButtonMenu({ name, onClick }: ButtonMenuProps) {
    return (
-      <button
-         onClick={onClick}
-         className="p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
-      >
-         {name}
+      <button onClick={onClick} className="w-[400px] h-[80px] p-[6px] bg-black">
+         <div className="h-full p-[4px] bg-gradient-to-b from-[#3E4EB1] to-[#1D265D]">
+            <div className="
+               flex
+               justify-center
+               items-center
+               h-full 
+               bg-gradient-to-b from-[#0F00B7] to-[#070056]
+               hover:from-[#1D265D] hover:to-[#3E4EB1] 
+               text-white
+               text-[28px] 
+               font-extrabold
+               text-stroke
+            ">
+               {name}
+            </div>
+         </div>
       </button>
    );
 }
--- src/components/MenuMain/index.tsx
+++ src/components/MenuMain/index.tsx
@@ -1,6 +1,7 @@
 import { ContextScene } from '@/contexts/context-scene';
 import { useContext } from 'react';
 import ButtonMenu from "./ButtonMenu";
+import Image from 'next/image';
 
 export default function MenuMain() {
    const { loadData, selectPage } = useContext(ContextScene)
@@ -18,11 +19,24 @@ export default function MenuMain() {
    };
 
    return (
-      <div className='flex flex-col pt-8 gap-4 items-center'>
-         <ButtonMenu name="Iniciar" onClick={handleStartGame} />
-         <ButtonMenu name="Carregar jogo" onClick={() => selectPage(3)} />
-         <ButtonMenu name="Abrir configurações" />
-         <ButtonMenu name="Sair do jogo" />
+      <div className='w-[1920px] h-[1080px] bg-green-900 '>
+         <div className="flex flex-col justify-center pl-[200px] pt-[50px] gap-[120px]">
+            <Image
+               width={630}
+               height={195}
+               src={'/icons/Logo.png'}
+               alt='Character image'
+            />
+
+            <div className="w-[500px] h-[680px] flex flex-col ml-[100px] gap-5 justify-center items-center bg-[#1D265D] border-[8px] border-black">
+               <ButtonMenu name="NOVO JOGO" onClick={handleStartGame} />
+               <ButtonMenu name="CARREGAR JOGO" onClick={() => selectPage(3)} />
+               <ButtonMenu name="CONQUISTAS" />
+               <ButtonMenu name="OPÇÕES" />
+               <ButtonMenu name="CREDITOS" />
+               <ButtonMenu name="SAIR" />
+            </div>
+         </div>
       </div>
    );
 }
