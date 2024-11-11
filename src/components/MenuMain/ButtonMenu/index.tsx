f3059202b98bd589aa8fb24dfe426120cf463199
Ciber-marcelo <marcelones125@gmail.com>
2024-11-11 16:28:37 -0300

    chore: edit ButtonMenu

--- src/components/MenuMain/ButtonMenu/index.tsx
+++ src/components/MenuMain/ButtonMenu/index.tsx
@@ -1,24 +1,25 @@
 type ButtonMenuProps = {
    name: string
    onClick?: any
+   disabled?: boolean
 }
 
-export default function ButtonMenu({ name, onClick }: ButtonMenuProps) {
+export default function ButtonMenu({ name, onClick, disabled }: ButtonMenuProps) {
    return (
-      <button onClick={onClick} className="w-[400px] h-[80px] p-[6px] bg-black">
+      <button onClick={onClick} className="w-[400px] h-[80px] p-[6px] bg-black" disabled={disabled}>
          <div className="h-full p-[4px] bg-gradient-to-b from-[#3E4EB1] to-[#1D265D]">
-            <div className="
+            <div className={`
                flex
                justify-center
                items-center
                h-full 
                bg-gradient-to-b from-[#0F00B7] to-[#070056]
-               hover:from-[#1D265D] hover:to-[#3E4EB1] 
+               ${disabled !== true && "hover:from-[#1D265D] hover:to-[#3E4EB1]"}
                text-white
                text-[28px] 
                font-semibold
-               opacity-[0.9]
-            ">
+               ${disabled === true ? "opacity-[0.1]" : "opacity-[0.9]"}
+            `}>
                {name}
             </div>
          </div>
